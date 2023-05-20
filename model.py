ee0074203ccc94f93511e3a2cab7d4473f1f47d7
Justin Lee <2025jlee@tjhsst.edu>
2023-05-20 13:07:26 -0400

    one more fix

--- model.py
+++ model.py
@@ -30,7 +30,7 @@ class Generator(nn.Module):
 
     def forward(self: Self, x: T.Tensor) -> T.Tensor:
         x = self.fully_connected_layers(x)
-        x = x.view(-1, 256, 15, 15)
+        x = x.view(-1, 128, 15, 15)
         x = self.inverse_convolutional_layers(x)
         x = self.convolutional_layers(x)
         return x
