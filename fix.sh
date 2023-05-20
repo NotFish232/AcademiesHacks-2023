6582d297169a18077adc3efc74ae86187ca546ba
Justin Lee <2025jlee@tjhsst.edu>
2023-05-20 15:35:15 -0400

    rm excess stuff
    Former-commit-id: 7a7afa5e198c8fc091f823f0284c93c1e498d50f [formerly 050f37736c42e48e04db41fa22ffde4d6dc81c80]
    Former-commit-id: f85638064c52db150dc80eff805b92117a7c2c1f

--- fix.sh
+++
@@ -1,9 +0,0 @@
-i=1
-for file in imgs/*; do
-    if [ $i -gt 10000 ]; then 
-        rm $file
-    else 
-        mv $file $i.png
-    fi
-    i=$(($i + 1))
-done
--- img/scripts/0166.py
+++
@@ -1,3 +0,0 @@
-from bing_image_downloader import downloader
-
-downloader.download("pirate image portrait", limit=1000,  output_dir='dataset', adult_filter_off=False, force_replace=False, timeout=60, verbose=True)
