48a4915b12d5139d2f9702bbbfe2dd6678720146
Justin Lee <2025jlee@tjhsst.edu>
2023-05-20 13:06:13 -0400

    okay maybe 1.7 bill on gen is too much

--- model.py
+++ model.py
@@ -7,21 +7,18 @@ from typing_extensions import Self
 class Generator(nn.Module):
     def __init__(self: Self) -> None:
         super().__init__()
-        # (batch_size, 2048)
+        # (batch_size, 1024)
         self.fully_connected_layers = nn.Sequential(
-            nn.Linear(2048, 15 * 15 * 128),  # (batch_size, 15 * 15 * 128)
-            nn.Dropout(),
-            nn.LeakyReLU(),
-            nn.Linear(15 * 15 * 128, 15 * 15 * 256),  # (batch_size, 15 * 15 * 256)
+            nn.Linear(1024, 15 * 15 * 128),  # (batch_size, 15 * 15 * 128)
             nn.Dropout(),
             nn.LeakyReLU(),
         )
         # (batch_size, 256, 15, 15)
         self.inverse_convolutional_layers = nn.Sequential(
-            nn.ConvTranspose2d(256, 128, 9, stride=4),  # (batch_size, 128, 65, 65)
+            nn.ConvTranspose2d(128, 64, 9, stride=4),  # (batch_size, 64, 65, 65)
             nn.Dropout2d(),
             nn.LeakyReLU(),
-            nn.ConvTranspose2d(128, 32, 8, stride=4),  # (batch_size, 32, 264, 264)
+            nn.ConvTranspose2d(64, 32, 8, stride=4),  # (batch_size, 32, 264, 264)
             nn.Dropout2d(),
             nn.LeakyReLU(),
         )
